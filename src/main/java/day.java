60b4610dc53f036e94fd3859592ac607f25ba941
彼方尚有荣光在 <424332385@qq.com>
2022-01-08 15:52:01 +0800

    第3次上传

---
+++ src/main/java/day.java
@@ -0,0 +1,11 @@
+/**
+ * @version 1.0
+ * @Author A_wei
+ * @Date 2022/1/8 15:51
+ * @注释
+ */
+public class day {
+    public static void main(String[] args) {
+        System.out.println("765878");
+    }
+}
